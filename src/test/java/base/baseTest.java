1090ec44194651b8c7b30a984415e94e425c13ec
KodeTester87 <naziyaa.nooreen@gmail.com>
2025-05-09 17:41:36 -0500

    first commit

--- src/test/java/base/baseTest.java
+++ src/test/java/base/baseTest.java
@@ -24,68 +24,68 @@ public class baseTest {
 	  private static AppiumDriver driver;
 	  private static AppiumDriverLocalService service;
 	  
-//	    public static AppiumDriverLocalService getService() {
-//	        return service;
-//	    }
+	    public static AppiumDriverLocalService getService() {
+	        return service;
+	    }
 	    
-//	    public static void startServer(final String platformName) throws Exception, Exception {
-//	    	String appiumJsPath = ConfigManager.getProperty("appium.js.path");
-//	        System.out.println("Appium JS Path is: " + appiumJsPath);
-//	        if (appiumJsPath == null || appiumJsPath.isEmpty()) {
-//	            throw new IllegalStateException("Appium JS Path is not set in environment variables.");
-//	        }  
-//	    String nodeExecutablePath = ConfigManager.getProperty("node.executable");
-//
-//	       Map<String, String> environment = new HashMap<>(System.getenv());
-//	        environment.put("ANDROID_SDK_ROOT",ConfigManager.getProperty("android.sdk.path") );
-//	        
-//	        // Create a builder for the Appium service
-//	        AppiumServiceBuilder builder = new AppiumServiceBuilder()
-//	        		  .withAppiumJS(new File(appiumJsPath))
-//	                  .usingDriverExecutable(new File(nodeExecutablePath))
-//	                  .withArgument(() -> "--use-plugins=appium-reporter-plugin,element-wait")
-//	                  .usingPort(4723)
-//	                  .withEnvironment(environment);
-//
-//	     
-//	        // Start the Appium server with the configured builder
-//	        service = AppiumDriverLocalService.buildService(builder);
-//	        service.start();
-//
-//	        // Log the server URL for debugging
-//	        System.out.println("Appium server started at " + service.getUrl());
-//	    
-//	    }
-//	    
-//	    public static void stopServer() {
-//            if (service != null && service.isRunning()) {
-//                service.stop();
-//                System.out.println("Appium server stopped.");
-//            } else {
-//                System.out.println("Appium server is not running.");
-//            }
-//         // Kill Appium Node process if still running
-//            try {
-//                String os = System.getProperty("os.name").toLowerCase();
-//                if (os.contains("mac")) {
-//                    Runtime.getRuntime().exec("killall node");
-//                } else if (os.contains("win")) {
-//                    Runtime.getRuntime().exec("taskkill /F /IM node.exe");
-//                }
-//                System.out.println("Appium (Node) process killed.");
-//            } catch (IOException e) {
-//                System.out.println("Failed to kill Appium process: " + e.getMessage());
-//            }
-//            
-//        }
+	    public static void startServer(final String platformName) throws Exception, Exception {
+	    	String appiumJsPath = ConfigManager.getProperty("appium.js.path");
+	        System.out.println("Appium JS Path is: " + appiumJsPath);
+	        if (appiumJsPath == null || appiumJsPath.isEmpty()) {
+	            throw new IllegalStateException("Appium JS Path is not set in environment variables.");
+	        }
+	    String nodeExecutablePath = ConfigManager.getProperty("node.executable");
+
+	       Map<String, String> environment = new HashMap<>(System.getenv());
+	        environment.put("ANDROID_SDK_ROOT",ConfigManager.getProperty("android.sdk.path") );
+
+	        // Create a builder for the Appium service
+	        AppiumServiceBuilder builder = new AppiumServiceBuilder()
+	        		  .withAppiumJS(new File(appiumJsPath))
+	                  .usingDriverExecutable(new File(nodeExecutablePath))
+	                  .withArgument(() -> "--use-plugins=appium-reporter-plugin,element-wait")
+	                  .usingPort(4723)
+	                  .withEnvironment(environment);
+
+
+	        // Start the Appium server with the configured builder
+	        service = AppiumDriverLocalService.buildService(builder);
+	        service.start();
+
+	        // Log the server URL for debugging
+	        System.out.println("Appium server started at " + service.getUrl());
+
+	    }
+
+	    public static void stopServer() {
+            if (service != null && service.isRunning()) {
+                service.stop();
+                System.out.println("Appium server stopped.");
+            } else {
+                System.out.println("Appium server is not running.");
+            }
+         // Kill Appium Node process if still running
+            try {
+                String os = System.getProperty("os.name").toLowerCase();
+                if (os.contains("mac")) {
+                    Runtime.getRuntime().exec("killall node");
+                } else if (os.contains("win")) {
+                    Runtime.getRuntime().exec("taskkill /F /IM node.exe");
+                }
+                System.out.println("Appium (Node) process killed.");
+            } catch (IOException e) {
+                System.out.println("Failed to kill Appium process: " + e.getMessage());
+            }
+
+        }
 	    
 	  public static void setup() throws MalformedURLException, Exception {
 		   
 	        String platform = ConfigManager.getProperty("platform").toLowerCase();
-	   //     startServer(platform);  
+	   startServer(platform);
 	        
 		  if (platform.equalsIgnoreCase("Android")) {
-			//  launchAndroidEmulator(ConfigManager.getProperty("avd.name"));
+			 launchAndroidEmulator(ConfigManager.getProperty("avd.name"));
 				UiAutomator2Options options = new UiAutomator2Options()
 						.setAppWaitActivity("*")						
 						 .setUdid(ConfigManager.getProperty("device.name"))
@@ -97,12 +97,12 @@ public class baseTest {
 				  .setFullReset(false);   // <-- Prevents uninstalling the app
 
 				 driver = new AndroidDriver(new URL(ConfigManager.getProperty("appium.server.url")), options);			
-				 driver.executeScript("plugin: setReporterPluginProperties", ImmutableMap.of(
-			                "enabled", true,
-			                "projectName", "Numpy Ninja Project",
-			                "reportTitle", "Appium Test Execution Report",
-			                "teamName", "Appium Avengers Team"
-			        ));
+//				 driver.executeScript("plugin: setReporterPluginProperties", ImmutableMap.of(
+//			                "enabled", true,
+//			                "projectName", "Numpy Ninja Project",
+//			                "reportTitle", "Appium Test Execution Report",
+//			                "teamName", "Appium Avengers Team"
+//			        ));
 				driver.executeScript("plugin: setWaitPluginProperties", ImmutableMap.of(
 					                "timeout", 10000,
 					                "intervalBetweenAttempts", 500
@@ -127,39 +127,39 @@ public class baseTest {
 	    }
 
 	  
-//	  private static void launchAndroidEmulator(String avdName) throws IOException, InterruptedException {
-//
-//		  String emulatorPath = ConfigManager.getProperty("android.emulator.path");
-//		    String adbPath = ConfigManager.getProperty("android.adb.path");
-//	        System.out.println("Starting Android Emulator: " + avdName);
-//	        // Start emulator process
-//	        ProcessBuilder emulatorPb = new ProcessBuilder(emulatorPath, "-avd", avdName,
-//	                "-wipe-data");
-//	        emulatorPb.redirectErrorStream(true);
-//	        emulatorPb.start();
-//
-//	        // Wait for emulator to be visible to adb
-//	        System.out.println("Waiting for device to boot...");
-//	        ProcessBuilder adbWaitPb = new ProcessBuilder(adbPath, "wait-for-device");
-//	        adbWaitPb.inheritIO();  // Optional: to show logs in console
-//	        Process waitProcess = adbWaitPb.start();
-//	        waitProcess.waitFor();  // This will block until the emulator is ready
-//
-//	        Thread.sleep(10000); 
-//	        System.out.println("Emulator is ready.");      
-//	    }
-//	  
-//	  
-//	  public static void stopAndroidEmulator() {
-//		    try {
-//		        String adbPath = ConfigManager.getProperty("android.adb.path");
-//		        ProcessBuilder pb = new ProcessBuilder(adbPath, "emu", "kill");
-//		        pb.start();
-//		        System.out.println("Emulator stopped.");
-//		    } catch (IOException e) {
-//		        System.out.println("Failed to stop emulator: " + e.getMessage());
-//		    }
-//		}
+	  private static void launchAndroidEmulator(String avdName) throws IOException, InterruptedException {
+
+		  String emulatorPath = ConfigManager.getProperty("android.emulator.path");
+		    String adbPath = ConfigManager.getProperty("android.adb.path");
+	        System.out.println("Starting Android Emulator: " + avdName);
+	        // Start emulator process
+	        ProcessBuilder emulatorPb = new ProcessBuilder(emulatorPath, "-avd", avdName,
+	                "-wipe-data");
+	        emulatorPb.redirectErrorStream(true);
+	        emulatorPb.start();
+
+	        // Wait for emulator to be visible to adb
+	        System.out.println("Waiting for device to boot...");
+	        ProcessBuilder adbWaitPb = new ProcessBuilder(adbPath, "wait-for-device");
+	        adbWaitPb.inheritIO();  // Optional: to show logs in console
+	        Process waitProcess = adbWaitPb.start();
+	        waitProcess.waitFor();  // This will block until the emulator is ready
+
+	        Thread.sleep(10000);
+	        System.out.println("Emulator is ready.");
+	    }
+
+
+	  public static void stopAndroidEmulator() {
+		    try {
+		        String adbPath = ConfigManager.getProperty("android.adb.path");
+		        ProcessBuilder pb = new ProcessBuilder(adbPath, "emu", "kill");
+		        pb.start();
+		        System.out.println("Emulator stopped.");
+		    } catch (IOException e) {
+		        System.out.println("Failed to stop emulator: " + e.getMessage());
+		    }
+		}
 	  
 	    @AfterMethod
 		public static void tearDown() {
--- src/test/java/context/TestContext.java
+++ src/test/java/context/TestContext.java
@@ -2,68 +2,77 @@ package context;
 
 import base.baseTest;
 import io.appium.java_client.AppiumDriver;
-import pages.SearchPage;
-import pages.SubscribePage;
-import pages.TrendingPage;
-import pages.videoPage;
-import pages.watchVideoCleanerPage;
+import pages.*;
 import utils.ExcelReader;
 
 public class TestContext {
 
-	private AppiumDriver driver;
+    private AppiumDriver driver;
     protected SubscribePage subscribePage;
     protected TrendingPage trendingPage;
     private ExcelReader excelReader;
     protected SearchPage searchPage;
     protected videoPage videoPage;
     private watchVideoCleanerPage watchVideoCleanerPage;
+    private DownloadPage downloadPage;
+    private BookmarkPage bookmarkPage;
 
 
-    
     public TestContext() throws Exception {
-        this.excelReader = new ExcelReader();  
+        this.excelReader = new ExcelReader();
     }
 
     public SubscribePage getSubscribePage() {
         if (this.subscribePage == null) {
-            this.subscribePage = new SubscribePage(getDriver()); 
+            this.subscribePage = new SubscribePage(getDriver());
         }
         return this.subscribePage;
     }
-    
-    
+
+
     public SearchPage getSearchPage() {
         if (this.searchPage == null) {
-            this.searchPage = new SearchPage(getDriver()); 
+            this.searchPage = new SearchPage(getDriver());
         }
         return this.searchPage;
     }
 
     public TrendingPage getTrendingPage() {
         if (this.trendingPage == null) {
-            this.trendingPage = new TrendingPage(getDriver()); 
+            this.trendingPage = new TrendingPage(getDriver());
         }
         return this.trendingPage;
     }
-    
+
+    public DownloadPage getDownloadPage() {
+        if (this.downloadPage == null) {
+            this.downloadPage = new DownloadPage(getDriver()); // Fixed constructor call
+        }
+        return this.downloadPage;
+    }
+
     public videoPage getvideoPage() {
         if (this.videoPage == null) {
-            this.videoPage = new videoPage(getDriver()); 
+            this.videoPage = new videoPage(getDriver());
         }
         return this.videoPage;
     }
-    
-    
+
+
     public watchVideoCleanerPage getwatchVideoCleanerPage() {
         if (this.watchVideoCleanerPage == null) {
-            this.watchVideoCleanerPage = new watchVideoCleanerPage(getDriver()); 
+            this.watchVideoCleanerPage = new watchVideoCleanerPage(getDriver());
         }
         return this.watchVideoCleanerPage;
     }
-    
 
-    
+    public BookmarkPage getBookmarkPage() {
+        if (this.bookmarkPage == null) {
+            this.bookmarkPage = new BookmarkPage(getDriver());
+        }
+        return this.bookmarkPage;
+    }
+
     public AppiumDriver getDriver() {
         if (this.driver == null) {
             this.driver = baseTest.getDriver();
