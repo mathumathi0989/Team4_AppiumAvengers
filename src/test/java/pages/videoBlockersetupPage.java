acab1a8573b0b30f1c1f34d60c0f0b1de5b4b9fd
AnusuyaSDET169 <anusuyas.official@gmail.com>
2025-05-12 10:48:05 -0500

    video blocker

---
+++ src/test/java/pages/videoBlockersetupPage.java
@@ -0,0 +1,117 @@
+package pages;
+
+import java.util.ArrayList;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import utils.reusableFunctions;
+
+public class videoBlockersetupPage {
+
+	private AppiumDriver driver;
+	reusableFunctions actions;
+
+	private By search = AppiumBy.accessibilityId("Search Videos");
+	private By enterSearch = AppiumBy
+			.xpath("//android.widget.AutoCompleteTextView[@resource-id=\"free.rm.skytube.oss:id/search_src_text\"]");
+	private By threeDots = AppiumBy
+			.xpath("(//android.widget.ImageButton[@resource-id=\"free.rm.skytube.oss:id/options_button\"])[1]");
+	// free.rm.skytube.oss:id/options_button
+	private By channel = AppiumBy
+			.xpath("//android.widget.TextView[@resource-id=\"free.rm.skytube.oss:id/title\" and @text=\"Channel...\"]");
+	// ByAndroidUIAutomator("new
+	// UiSelector().resourceId(\"free.rm.skytube.oss:id/title\").textContains(\"Channel\")")
+	private By blockChannel = AppiumBy.xpath(
+			"//android.widget.TextView[@resource-id=\"free.rm.skytube.oss:id/title\" and @text=\"Block Channel\"]");
+	private By preferences = AppiumBy.xpath(
+			"//android.widget.TextView[@resource-id=\"free.rm.skytube.oss:id/title\" and @text=\"Preferences\"]");
+	private By videoBlockerText = AppiumBy
+			.xpath("//android.widget.TextView[@resource-id=\"android:id/title\" and @text=\"Video Blocker\"]");
+
+	private By ChannelDenyList = AppiumBy
+			.xpath("//android.widget.TextView[@resource-id=\"android:id/title\" and @text=\"Channel Deny List\"]");
+	private By denyListChannels = AppiumBy.accessibilityId("free.rm.skytube.oss:id/channel_name");
+
+	private By selectAlltoUnblock = AppiumBy.accessibilityId("free.rm.skytube.oss:id/select_all_button");
+
+	private By Unblock = AppiumBy.accessibilityId("free.rm.skytube.oss:id/md_buttonDefaultPositive");
+
+	private By back = AppiumBy.accessibilityId("Navigate up");
+
+	public videoBlockersetupPage(AppiumDriver driver) {
+		this.driver = driver;
+		actions = new reusableFunctions(driver, 30);
+	}
+
+	public void clickThreeDots() {
+		driver.findElement(threeDots).click();
+	}
+
+	public void clickChannel() {
+		driver.findElement(channel).click();
+	}
+
+	public boolean isOptionVisible(String expectedText) {
+		return driver.findElement(By.xpath("//android.widget.TextView[@text='" + expectedText + "']")).isDisplayed();
+	}
+
+	public void clickBlockChannel() {
+		driver.findElement(blockChannel).click();
+		;
+	}
+
+	public void clickPreferences() {
+		driver.findElement(preferences).click();
+	}
+
+	public void clickvideoBlockoption() {
+		driver.findElement(videoBlockerText).click();
+
+	}
+
+	public void viewChannelDenyList() {
+		driver.findElement(ChannelDenyList).click();
+
+	}
+
+	public List<String> getBlockedChannelNames() {
+		List<String> channelNames = new ArrayList<>();
+		List<WebElement> channelElements = driver.findElements(By.id("free.rm.skytube.oss:id/channel_name"));
+		for (WebElement element : channelElements) {
+			channelNames.add(element.getText().trim());
+		}
+		return channelNames;
+	}
+
+	public void selectAllBlockedChannels() {
+
+		driver.findElement(selectAlltoUnblock).click();
+
+	}
+
+	public void unblockChannel() {
+
+		driver.findElement(Unblock).click();
+	}
+	  public void clickBack() {
+		   driver.findElement(back).click();
+	   }
+	  
+	  public void enterSearchData() {
+	    	driver.findElement(search).click();
+	    	driver.findElement(search).sendKeys("young thug");
+	    	  // Cast WebDriver to AndroidDriver to access pressKey
+	        AndroidDriver androidDriver = (AndroidDriver) driver;
+	        androidDriver.pressKey(new KeyEvent(AndroidKey.ENTER));
+	    }
+	    
+	
+
+}
--- src/test/resources/features/videoBlocker.feature
+++ src/test/resources/features/videoBlocker.feature
@@ -5,28 +5,25 @@ Feature: Video Blocker Setup Functionality
  Background:  
    Given    User launched the app 
 
-  @VideoBlockFunctionality
-  
+  @VideoBlockFunctionality  
   Scenario: Verify the block channel option in search results
-    Given User is on the Search tab
+    Given User is on the Search tab to block channel
     When User searches for a specific channel
     And User clicks the three-dot menu for that channel
     Then User should see the "Block Channel" option
  
    @VideoBlockFunctionality
-    
- 
   Scenario: Validate adding a channel to the block list
     Given User is on the Video Blocker settings page
     When User selects either the Deny List or Allow List
     Then User should see videos listed in the Block List
   
-   @VideoBlockFunctionality
-    
-  Scenario: Validate disabling Video Blocker to show all videos
-    Given Video Blocker is currently enabled in settings
-    When User disables the Video Blocker toggle
-    Then Blocked videos should now be visible in the list
+   @VideoBlockFunctionality  
+  Scenario: Validate unblocking channel from blocklist
+  
+    Given User is on the Video Blocker settings page to unblock
+    When User unblocks either the Deny List or Allow List
+    Then User should see videos in playlist
  
 
  
